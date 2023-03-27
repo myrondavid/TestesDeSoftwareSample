88564564d5108ae6946bcdb8bfc155bbe4d24c77
Audrey <aerv@ic.ufal.br>
2023-03-27 07:43:02 -0300

    Add testes com nUnit

--- SampleNUnit/ContaBancoTests.cs
+++ SampleNUnit/ContaBancoTests.cs
@@ -4,16 +4,57 @@ namespace SampleNUnit
 {
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
+       
         }
 
         [Test]
-        public void Test1()
+        public void TestCliente()
         {
-            ContaBanco c = new ContaBanco("Cliente1", 1000);
+            ContaBanco c = new ContaBanco("Pessoa", 1000);
+            Assert.IsNotNull(c);
+            Assert.That(c.Saldo, Is.EqualTo(1000));
             Assert.Pass();
+        }
+
+        [Test]
+        public void TestSaque()
+        {
+            double valorInicial = 5000;
+            double valorSaque = 2000;
+            double valorEsperado = valorInicial - valorSaque;
+
+            ContaBanco c = new ContaBanco("Pessoa", valorInicial);
+
+            c.Saque(valorSaque);
+            double valor_atual = c.Saldo;
+            Assert.That(valor_atual, Is.EqualTo(valorEsperado));
+
+        }
+
+        [Test]
+        public void TestDeposito()
+        {
+            ContaBanco c = new ContaBanco("Pessoa", 1000);
+            c.Deposito(1000);
+            Assert.That(c.Saldo, Is.EqualTo(2000));
+        }
+
+        [Test]
+        public void TestLog()
+        {
+            ContaBanco c = new ContaBanco("Pessoa", 1200);
+            c.Deposito(100);
+            c.Deposito(200);
+            c.Saque(50);
+            c.Saque(150);
+            c.Deposito(100);
+
+            var qntOperacoes = c.LogOperacoes.Count;
+            Assert.That(qntOperacoes, Is.EqualTo(5));
         }
     }
 }
